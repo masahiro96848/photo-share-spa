04eb779af68056f8b8e33b3961a42f6738171ee5
masahiro96848 <feetwcpic9436@gmail.com>
2021-06-06 13:30:07 +0900

    vuexを導入　CSRF対策を導入

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -1,5 +1,7 @@
-window._ = require('lodash');
+const { getCookieValue } = require('./util');
 
+window._ = require('lodash');
+import {getCookieValue} from './util'
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -8,8 +10,15 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
+// Ajaxリクエストであることを示すヘッダーを付与する
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.axios.interceptors.request.use(config => {
+  // クッキーからトークンを取り出してヘッダーに添付する
+  config.headers['X-XSRF-TOKEN'] = getCookieValue('XSRF-TOKEN')
+
+  return config
+})
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
